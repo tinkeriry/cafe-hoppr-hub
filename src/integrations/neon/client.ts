4f16045eafe78f6031205889b156480b2936bd42
M Azhar Alauddin <muhammadazharalauddin@gmail.com>
2025-11-02 10:13:19 +0700

    fix: disableWarningInBrowsers on neon console

--- src/integrations/neon/client.ts
+++ src/integrations/neon/client.ts
@@ -1,5 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 
-const sql = neon(import.meta.env.VITE_DATABASE_URL!);
+const sql = neon(import.meta.env.VITE_DATABASE_URL!, {
+  disableWarningInBrowsers: true,
+});
 
 export { sql };
